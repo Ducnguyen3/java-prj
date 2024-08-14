0cf3aba90eec343db7ed5c02fb80c20f7d6a8fb8
DucNguyen3 <23732881.duc@student.iuh.edu.vn>
2024-08-15 00:05:28 +0700

    first prj

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome{
+    public static void main(String[] args){
+        System.out.println("Hello Nguyen Ba Duc");
+        System.out.println("Hello to the DHKTPM19B");
+    }
+}
